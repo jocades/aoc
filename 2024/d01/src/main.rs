8e3951d2b1499090f6f0ba8cf76210035bf5837f
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2024-12-02 15:00:04 +0100

    d1p2

--- 2024/d01/src/main.rs
+++ 2024/d01/src/main.rs
@@ -1,5 +1,3 @@
-const INPUT: &'static str = include_str!("input");
-
 const TEST: &'static str = "3   4
 4   3
 2   5
@@ -7,20 +5,41 @@ const TEST: &'static str = "3   4
 3   9
 3   3";
 
-fn main() {
-    let mut l1 = Vec::new();
-    let mut l2 = Vec::new();
+const INPUT: &'static str = include_str!("input");
 
-    for line in INPUT.lines() {
+fn parse(input: &str, l1: &mut Vec<i32>, l2: &mut Vec<i32>) {
+    for line in input.lines() {
         let mut iter = line.split_whitespace();
-        l1.push(iter.next().unwrap().parse::<i32>().unwrap());
-        l2.push(iter.next().unwrap().parse::<i32>().unwrap());
+        l1.push(iter.next().unwrap().parse().unwrap());
+        l2.push(iter.next().unwrap().parse().unwrap());
     }
+}
+
+fn part1(input: &str) -> i32 {
+    let (mut l1, mut l2) = (Vec::new(), Vec::new());
+    parse(input, &mut l1, &mut l2);
 
     l1.sort();
     l2.sort();
 
-    let output: i32 = l1.iter().zip(&l2).map(|(&a, &b)| (a - b).abs()).sum();
+    l1.iter().zip(&l2).map(|(&a, &b)| (a - b).abs()).sum()
+}
 
-    println!("{output}");
+fn part2(input: &str) -> i32 {
+    let (mut l1, mut l2) = (Vec::new(), Vec::new());
+    parse(input, &mut l1, &mut l2);
+
+    // O(len(l1) * len(l2))
+    let mut sym = Vec::new();
+    for &x in l1.iter() {
+        let n = l2.iter().filter(|&&y| x == y).count();
+        sym.push(x * n as i32);
+    }
+
+    sym.iter().sum()
+}
+
+fn main() {
+    println!("part1 = {}", part1(INPUT));
+    println!("part2 = {}", part2(INPUT));
 }
