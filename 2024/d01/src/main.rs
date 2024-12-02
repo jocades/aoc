6d9d391c28fa96c88b0eada359dcd35c7a32d382
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2024-12-02 16:21:08 +0100

    vs simple cache

--- 2024/d01/src/main.rs
+++ 2024/d01/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 const TEST: &'static str = "3   4
 4   3
 2   5
@@ -30,16 +32,46 @@ fn part2(input: &str) -> i32 {
     parse(input, &mut l1, &mut l2);
 
     // O(len(l1) * len(l2))
-    let mut sym = Vec::new();
-    for &x in l1.iter() {
-        let n = l2.iter().filter(|&&y| x == y).count();
-        sym.push(x * n as i32);
-    }
+    // PERF: Simple cache.
+    l1.iter()
+        .map(|x| {
+            let n = l2.iter().filter(|&y| x == y).count();
+            x * n as i32
+        })
+        .sum()
+}
 
-    sym.iter().sum()
+fn part2_with_cache(input: &str) -> i32 {
+    let (mut l1, mut l2) = (Vec::new(), Vec::new());
+    parse(input, &mut l1, &mut l2);
+
+    let mut cache = HashMap::new();
+    l1.iter()
+        .map(|x| {
+            if let Some(&v) = cache.get(x) {
+                v
+            } else {
+                let n = l2.iter().filter(|&y| x == y).count();
+                let v = x * n as i32;
+                cache.insert(x, v);
+                v
+            }
+        })
+        .sum()
+}
+
+macro_rules! timeit {
+    ($f:expr) => {{
+        let now = std::time::Instant::now();
+        let res = $f;
+        (now.elapsed(), res)
+    }};
 }
 
 fn main() {
     println!("part1 = {}", part1(INPUT));
-    println!("part2 = {}", part2(INPUT));
+    let (t1, r1) = timeit!(part2(INPUT));
+    let (t2, r2) = timeit!(part2_with_cache(INPUT));
+    assert_eq!(r1, r2);
+    println!("part2 = {t1:?} part2_cache = {t2:?}");
 }
