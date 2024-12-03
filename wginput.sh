84165c1c04e97a2089b8bf8efcd6e0206ba5a2e2
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2024-12-03 10:03:30 +0100

    simple get input shell script

---
+++ wginput.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+curl "https://adventofcode.com/2024/day/$1/input" \
+    -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+    -H 'accept-language: en-US,en;q=0.9' \
+    -H 'cache-control: no-cache' \
+    -H "cookie: session=$AOC_AUTH_COOKIE" \
+    -H 'dnt: 1' \
+    -H 'pragma: no-cache' \
+    -H 'priority: u=0, i' \
+    -H 'referer: https://adventofcode.com/2024/day/3' \
+    -H 'sec-ch-ua: "Not?A_Brand";v="99", "Chromium";v="130"' \
+    -H 'sec-ch-ua-mobile: ?0' \
+    -H 'sec-ch-ua-platform: "macOS"' \
+    -H 'sec-fetch-dest: document' \
+    -H 'sec-fetch-mode: navigate' \
+    -H 'sec-fetch-site: same-origin' \
+    -H 'sec-fetch-user: ?1' \
+    -H 'upgrade-insecure-requests: 1' \
+    -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36' \
+    -o input
